3bd8d1f58c7cc89a8db33e7354e17173cac95712
unknown <lixuan.lim.2016@sis.smu.edu.sg>
2018-11-05 15:49:36 +0800

    pdf

--- api.py
+++ api.py
@@ -1,19 +1,60 @@
-from flask import Flask
+from flask import Flask,request,jsonify,flash,session
 from flask_restful import Api
-from controllers import helloController
-from flask_sqlalchemy import SQLAlchemy
+
+import tempfile
+import os
+import tika
+import regex as re
+tika.initVM()
+from tika import parser
+
+
 
 app = Flask(__name__)
-
-api = Api(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.secret_key = "super secret key"
 
 
-db = SQLAlchemy(app)
+print("Start api fie")
 
-api.add_resource(helloController.HelloController, '/api/hello')
+@app.route("/")
+def hello():
+    return "Hello World!"
+UPLOAD_FOLDER = './dump/'   
+ALLOWED_EXTENSIONS = set([ 'pdf'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/pdftotext', methods=['POST'])
+def upload_file():
+    try:
+        print("lol")
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+        file = request.files['file']
+        # if user does not select file, browser also
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "data.pdf"))
+
+    except: 
+        output="error"
+        print("check if saved")
+
+    try:
+        parsed = parser.from_file('./dump/data.pdf')
+        print(parsed["metadata"])
+        print(parsed["content"])
+        output = parsed["content"]
+        output = output.replace("\r","")
+        output = output.replace("\n","")
+        output=output.replace("\s+|^[ \t]+|[ \t]+$"," ")
+    except: 
+        output= parsed["metadata"] 
+        print("check if text is returned")
+
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True)
